c63deea7bb1a91e7a615d9576d255595446b264e
Sravankumar1612 <sravankumartikkavarapu@gmail.com>
2023-07-11 11:58:45 +0530

    First commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gotoexample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username;
+            again:
+            Console.WriteLine("Enter user name");
+            username = Console.ReadLine();
+            if(username.Length >=6) {
+                Console.WriteLine("Welcome  " + username);
+            }
+            else
+            {
+                Console.WriteLine("inavalid username");
+                goto again;
+            }
+            Console.ReadKey();
+        }
+    }
+}
